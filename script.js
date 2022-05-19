1eb409c323ad27505458e25a4dcd1a63a1bbc775
janupasilva2005 <janupasilva@gmail.com>
2022-05-19 17:50:34 +0530

    Created the basic features and command entering

--- script.js
+++ script.js
@@ -29,10 +29,100 @@ const colorSchemes = {
   },
 };
 
-// Terminal container
-const mainContainer = document.getElementById("container");
+const selectedColor = colorSchemes.solarizedDark;
+
+const headerString = "janupasilva@gmail.com";
+
+const helpCommands = [
+  "history - Shows the previous commands entered",
+  "info - Shows the info the creator",
+  "color - Change the color schems",
+  "version - Show the version",
+];
+
+const input = document.getElementById("input"); // User input
+const historyContainer = document.getElementById("history");
+const headerElement = document.getElementById("header");
+
+// Array to store all the commands user entered
+let history = [];
+
+/**
+ * Create the output
+ */
+const createHistory = (command) => {
+  history.push(command); // Adding to history
+
+  const historyItem = document.createElement("div"); // Creating history item
+
+  // const header = document.createElement("p");
+  // header.style.color = "#00CD00";
+  // header.innerHTML = headerString;
+
+  historyItem.innerHTML = `<p>${command}</p>`;
+
+  historyContainer.append(historyItem); // Adding the item to top history
+};
+
+const setInputEmpty = (e) => {
+  // Setting the input field to null
+  e.target.value = "";
+};
+
+const showHeader = (command) => {
+  const header = document.createElement("p");
+  header.style.color = "#00CD00";
+  header.innerHTML =
+    headerString +
+    ":~$ " +
+    `<span color='${selectedColor.text}'>${command}</span>`;
+
+  historyContainer.append(header);
+};
+
+// Input event function
+input.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    switch (e.target.value) {
+      case "help":
+        showHeader("help");
+
+        helpCommands.forEach((cmd) => {
+          createHistory(cmd);
+        });
+        setInputEmpty(e);
+
+        return;
+      case "info":
+        showHeader("info");
+
+        createHistory("show info");
+        setInputEmpty(e);
+
+        return;
+
+      case "version":
+        showHeader("version");
+
+        createHistory("show the version");
+        setInputEmpty(e);
+
+        return;
+      default:
+        showHeader(e.target.value);
+
+        createHistory("Command Not found");
+        setInputEmpty(e);
+
+        return;
+    }
+  }
+});
 
 window.addEventListener("DOMContentLoaded", () => {
   // Set the window color to default color
-  mainContainer.style.backgroundColor = colorSchemes.solarizedDark.background;
+  document.body.style.backgroundColor = selectedColor.background;
+
+  headerElement.style.color = "#00CD00";
+  headerElement.innerHTML = headerString + " :~$";
 });
